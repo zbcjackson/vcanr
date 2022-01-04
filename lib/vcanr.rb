eb09af5348671326915f21fdccd35b9c9af2fe5a
zbcjackson <zbcjackson@gmail.com>
2022-01-04 17:05:41 +0800

    Init repo

---
+++ lib/vcanr.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "vcanr/version"
+
+module Vcanr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/vcanr_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Vcanr do
+  it "has a version number" do
+    expect(Vcanr::VERSION).to be "0.1.0"
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
