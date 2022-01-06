dc8ae4940d6d26bcd04c3e03226abfbba028007a
zbcjackson <zbcjackson@gmail.com>
2022-01-06 20:56:02 +0800

    Add spec for churn analyzer

--- spec/vcanr/churn_analyzer_spec.rb
+++ spec/vcanr/churn_analyzer_spec.rb
@@ -1,6 +1,17 @@
 require "rspec"
 
 module Vcanr
+  def commit_with(*deltas)
+    commit = Commit.new
+    deltas.each do |d|
+      delta = Delta.new
+      delta.file = d.is_a?(Hash) ? d[:file] : d
+      delta.status= d.is_a?(Hash) ? d[:status] : :modified
+      commit.deltas << delta
+    end
+    commit
+  end
+
   describe "Vcanr::ChurnAnalyzer" do
     before do
       @repo_accessor = instance_double(GitRepoAccessor)
@@ -8,13 +19,26 @@ module Vcanr
       @churn_analyzer = ChurnAnalyzer.new @repo_accessor, @reporter
     end
 
-    context "no commits" do
-      it "returns empty stat" do
-        allow(@repo_accessor).to receive(:commits) { [] }
-        @churn_analyzer.analyze
-        @churn_analyzer.report
-        expect(@reporter).to have_received(:report).with({})
-      end
+    it "show empty stat when no commits" do
+      allow(@repo_accessor).to receive(:commits) { [] }
+      @churn_analyzer.analyze
+      @churn_analyzer.report
+      expect(@reporter).to have_received(:report).with({})
     end
+
+    it "count file changes" do
+      allow(@repo_accessor).to receive(:commits) { [commit_with("a.txt", "b.txt"), commit_with("a.txt")] }
+      @churn_analyzer.analyze
+      @churn_analyzer.report
+      expect(@reporter).to have_received(:report).with({"a.txt" => 2, "b.txt" => 1})
+    end
+
+    it "remove file stat when file change is deleted" do
+      allow(@repo_accessor).to receive(:commits) { [commit_with("a.txt", "b.txt"), commit_with({file:"a.txt", status: :deleted})] }
+      @churn_analyzer.analyze
+      @churn_analyzer.report
+      expect(@reporter).to have_received(:report).with({"b.txt" => 1})
+    end
+
   end
 end
